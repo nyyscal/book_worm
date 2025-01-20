5b1bb3685625317cfd0c7618e0c05bbf8fe46268
Nischal <nyyscal541556@gmail.com>
2025-01-20 12:27:37 +0545

    feat(auth) : login/signup functionality added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont  from "next/font/local"
 import { ReactNode } from "react";
+import { Toast } from "@/components/ui/toast";
+import { Toaster } from "@/components/ui/toaster";
 
 const imbPlexSans = localFont({
  src:[
@@ -32,6 +34,7 @@ const RootLayout=(
         className={`${imbPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+        <Toaster/>
       </body>
     </html>
   );
