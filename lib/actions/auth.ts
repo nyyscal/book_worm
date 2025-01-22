b0a1c3efc07e3eb71a31549de8004c7bf3f35fb0
Nischal <nyyscal541556@gmail.com>
2025-01-22 11:37:57 +0545

    feat(Rate Limiter Set) : Performance Optimization

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,9 +5,17 @@ import { eq } from "drizzle-orm";
 import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
+import { headers } from "next/headers";
+import ratelimit from "../ratelimit";
+import { redirect } from "next/navigation";
 
 export const signInWithCredentials = async(params:Pick<AuthCredentials,"email"|"password">) =>{
   const {email,password} = params;
+
+  const ip = (await headers()).get("x-forwarded-for")|| "127.0.0.1"
+  const {success} = await ratelimit.limit(ip)
+  if(!success) return redirect("/too-fast")
+    
   try {
     const result = await signIn("credentials",{email,password,redirect:false});
     if(result?.error){
@@ -20,7 +28,12 @@ export const signInWithCredentials = async(params:Pick<AuthCredentials,"email"|"
 }
 
 export const signUp = async(params:AuthCredentials) =>{
+
   const {fullName,email,universityId,universityCard,password} = params;
+
+  const ip = (await headers()).get("x-forwarded-for")|| "127.0.0.1"
+  const {success} = await ratelimit.limit(ip)
+  if(!success) return redirect("/too-fast")
 
   const existingUser = await db
   .select()
