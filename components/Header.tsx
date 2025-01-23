71bb0bd68bd3ca91b37e5e1b9dbbb31481c69573
Nischal <nyyscal541556@gmail.com>
2025-01-23 16:28:37 +0545

    Deployment Error Fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,31 @@
-
-import { cn, getInitials } from '@/lib/utils'
-import Image from 'next/image'
-import Link from 'next/link'
-// import { usePathname } from 'next/navigation'
-import React from 'react'
-import { signOut } from 'next-auth/react'
-import { Button } from './ui/button'
+import Link from "next/link";
+import Image from "next/image";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-  // const pathname = usePathname()
   return (
-    <header className='my-10 flex justify-between gap-5'>
-      <Link href="/"><Image src="/icons/logo.svg" alt="logo" width={40} height={40}/></Link>
-      <ul className='flex flex-row items-center gap-8'>
-        {/* {/* <li><Link href="/library" className={cn("text-base cursor-pointer capitalize",pathname === "/library" ? "text-light-200":"text-light-100")}>Library</Link></li> */}
-        <li> 
-        <form action={async ()=>{
-        "use server";
-        await signOut()
-      }}
-      className='mb-10'
-      >
-        <Button>Log out</Button>
-      </form>
-          {/* <Link href="/my-profile">
-          <Avatar>
-            <AvatarFallback className='bg-amber-100'>{getInitials(session?.user?.name ||"IN")}</AvatarFallback>
-          </Avatar>
-          </Link> */}
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
+
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut();
+            }}
+            className="mb-10"
+          >
+            <Button>Logout</Button>
+          </form>
         </li>
       </ul>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
